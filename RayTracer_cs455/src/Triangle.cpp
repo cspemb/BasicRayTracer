e3c10bb5b3d7a838b75e7b5f2c2dbc4eb5e195dc
Connor Pemberton <pemberton.csp@gmail.com>
2023-03-07 10:20:27 -0700

    Add Triangle SceneObject

---
+++ RayTracer_cs455/src/Triangle.cpp
@@ -0,0 +1,13 @@
+#include "Triangle.h"
+
+float Triangle::intersect(std::shared_ptr<Ray> r)
+{
+    // TODO replace stub
+    return 1.0f;
+}
+
+glm::vec3 Triangle::getNormal(glm::vec3 point)
+{
+    // TODO replace stub
+    return {1.0f, 1.0f, 1.0f};
+}
